e0d95cbcce1bc70755dc670dbac308c0167f28ee
caue <cauesouzadelima7@gmail.com>
2023-08-05 13:44:51 -0300

    initial commit

---
+++ futebol/dados/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Clube(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Jogador(models.Model): 
+    foto = models.ImageField(upload_to='static/', blank=True, null=True)
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    clube = models.ForeignKey(Clube, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.nome 
+
+class Dados(models.Model): 
+    jogador = models.ForeignKey(Jogador, on_delete=models.CASCADE)
+    temporada = models.IntegerField(default=0)
+    partidas = models.IntegerField(default=0)
+    gols = models.IntegerField(default=0)
+    assistências = models.IntegerField(default=0)
